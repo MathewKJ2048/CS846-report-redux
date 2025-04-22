41e14dabe1ba3f46dc28788cee96a985dc0b868d
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-04-22 06:23:44 -0400

    library functions

--- src/lib.py
+++ src/lib.py
@@ -15,3 +15,25 @@ def extract_thoughts(text):
 	m = re.search(r"<think>([^<]*)</think>",text,re.DOTALL)
 
 print(extract_code("```\n\n\naaabbbcc```"))
+
+def read_file(path):
+	with open(path) as f:
+		data = f.read()
+	return data
+
+def process_response(path):
+	data = read_file(path)
+	result = {
+		"think":None,
+		"code":None,
+		"text":None
+	}
+	try:
+		m = re.search(r"<think>([^>]*)</think>(.*)",data,re.DOTALL)
+		result["think"] = m.group(1)
+		result["text"] = m.group(2)
+		m = re.search(r"```(.*)```",result["text"],re.DOTALL)
+		result["code"] = m.group(1)
+	except:
+		pass
+	return result
